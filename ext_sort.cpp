55a1f1ca70321a1f681ca378a96eb2652b7740b3
159aadi314 <73881267+159aadi314@users.noreply.github.com>
2022-04-25 13:49:32 +0530

    Add files via upload

--- ext_sort.cpp
+++ ext_sort.cpp
@@ -63,64 +63,56 @@ int main() {
   }
   int run_size = 1;
   int num_iterations = (int) ceil((double)num_pages/(double)B);
-  for(int i = 0; i < num_iterations; i+=B) {                                     // Phase 0
+  for(int i = 0; i < num_iterations; i++) {                                     // Phase 0
     vector<int> recs_to_be_sorted;
-    for(int j = i; j < i+B; j++){
+    for(int j = i*B; j < min(num_pages, i*B+B); j++){
+      //data_file[j-1]->getcontents();
       for(int k = 0; k < data_file[j]->getcontents().size(); k++){
         recs_to_be_sorted.push_back(data_file[j]->getcontents()[k]);
       }
     }
     sort(recs_to_be_sorted.begin(), recs_to_be_sorted.end());
-    left = recs_to_be_sorted.size();
-    // if(i==0){
-    //   for(int u = 0; u < recs_to_be_sorted.size();u++){
-    //     cout << recs_to_be_sorted[u] << "d";
-    //   }
-    // }
     int f = 0;
-    for(int j = i; j < i+B; j++) {                            
-      for(int k = 0; k < min(left, page_capacity); k++) {
+    for(int j = i*B; j < min(num_pages,i*B+B); j++) {                            
+      for(int k = 0; k < page_capacity; k++) {
         data_file[j]->setcontents(k, recs_to_be_sorted[f]);
         f++;
-        left--;
       }
     }
   }
   run_size = B;
   int num_runs = num_iterations;
 
-  // while(run_size < num_pages){
-  //   num_iterations = (int)ceil(((double)num_runs)/(double)(B-1));
-  //   for(int i = 0; i < num_iterations; i+=B-1) {
-  //     vector<int> recs_to_be_sorted;
-  //     for(int j = i; j < i + B - 1; j++){
-  //       for(int l = j; l < j + run_size; l++){
-  //         for(int k = 0; k < data_file[l]->getcontents().size(); k++){
-  //           recs_to_be_sorted.push_back(data_file[l]->getcontents()[k]);
-  //         }
-  //       }
-  //     }
-  //     sort(recs_to_be_sorted.begin(), recs_to_be_sorted.end());
-  //     left = recs_to_be_sorted.size();
-  //     int f = 0;
-  //     for(int j = i; j < i+B; j++) { 
-  //       for(int l = j; l < j + run_size; l++){
-  //         for(int k = 0; k < min(left, page_capacity); k++) {
-  //           data_file[l]->setcontents(k, recs_to_be_sorted[f]);
-  //           f++;
-  //           left--;
-  //         } 
-  //       }                            
-  //     }
-  //   }
-  //   num_runs = num_iterations;
-  //   run_size *= B-1;
-  // }
+  while(run_size < num_pages){
+    num_iterations = (int)ceil(((double)num_runs)/(double)(B-1));
+    for(int i = 0; i < num_iterations; i++) {
+      vector<int> recs_to_be_sorted;
+      for(int j = i*(B-1); j < min(num_runs, i*(B-1) + B - 1); j++){
+        for(int l = j*run_size; l < min(num_pages,j*run_size + run_size); l++){
+          for(int k = 0; k < data_file[l]->getcontents().size(); k++){
+            recs_to_be_sorted.push_back(data_file[l]->getcontents()[k]);
+          }
+        }
+      }
+      sort(recs_to_be_sorted.begin(), recs_to_be_sorted.end());
+      int f = 0;
+      for(int j = i*(B-1); j < min(num_runs, i*(B-1)+B-1); j++) { 
+        for(int l = j*run_size; l < min(num_pages,j*run_size + run_size); l++){
+          for(int k = 0; k < page_capacity; k++) {
+            data_file[l]->setcontents(k, recs_to_be_sorted[f]);
+            f++;
+          } 
+        }                            
+      }
+    }
+    num_runs = num_iterations;
+    run_size *= B-1;
+  }
 
   for(int i = 0; i < num_pages; i++) {
     for(int j = 0; j < data_file[i]->occupancy; j++) {
-      cout << data_file[i]->getcontents()[j] << " ";
-    }cout << endl;
+      cout << data_file[i]->getcontents()[j] << endl;
+    }
   }
 
 }
